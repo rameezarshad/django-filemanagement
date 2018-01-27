3bbc7303911b748ca80a3860311c3bf0d9d58726
Rameez <rameez.ars740@gmail.com>
2018-01-27 20:27:30 +0530

    URLconf updated

--- filemanagement/urls.py
+++ filemanagement/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^', include('filemanager.urls', namespace='filemanager'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
